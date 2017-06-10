07d894081877500e491115da111ef2f20ff2dc35
Administrador <Administrador@10.10.3.3>
2017-06-10 09:06:03 -0300

    ola mundo

--- conexaoBanco/src/conexaobanco/ConexaoBanco.java
+++ conexaoBanco/src/conexaobanco/ConexaoBanco.java
@@ -7,7 +7,7 @@ public class ConexaoBanco {
    
     public static void main(String[] args) {
         
-        System.out.println("Olá mundo!");
+        System.out.println("Olá, mundo!");
         
     }
     
